0a5c69a33328e688ac69995024e20d4cdc2f8013
Jeremy C. Zacharia <jczacharia@gmail.com>
2023-07-04 13:23:35 -0400

    remove fk attribute

--- Domain/Entities/Contestant.cs
+++ Domain/Entities/Contestant.cs
@@ -1,17 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace Domain.Entities;
+﻿namespace Domain.Entities;
 
 public class Contestant
 {
     public int Id { get; init; }
-
-    [ForeignKey(nameof(UserId))]
     public required User User { get; set; }
-
-    [ForeignKey(nameof(ContestId))]
     public required Contest Contest { get; set; }
-
-    public int UserId { get; }
-    public int ContestId { get; }
 }
